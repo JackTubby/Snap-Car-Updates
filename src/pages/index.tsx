93c909d7da28b234e9407a6044369db8e8d46789
JackTubby <jack200034@hotmail.com>
2023-03-23 21:40:16 +0000

    Import & display navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+import Navbar from "../components/Navbar"
+
 export default function Home() {
   return (
     <>
@@ -14,7 +16,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Navbar />
+      </main>
     </>
   )
 }
