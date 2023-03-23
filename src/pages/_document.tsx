ea540c89c96a3799fe233f4a6200e87b93f9567e
JackTubby <jack200034@hotmail.com>
2023-03-23 21:40:02 +0000

    Add google fonts Ubuntu & Open Sans

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
