25b60dcde6511d56ca76414bd6e05e1a98215101
JackTubby <jack200034@hotmail.com>
2023-03-24 23:00:52 +0000

    Init

---
+++ src/components/sidebars/Closed.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Closed = () => {
+  return (
+    <>
+      <div>
+        <p>Notifications</p>
+        <p>Feed</p>
+        <p>Stats</p>
+        <p>Calendar</p>
+      </div>
+      <div>
+        <p>Get Help</p>
+        <p>Submit Feedback</p>
+      </div>
+      <div>
+        <p>Profile</p>
+        <p>Sign Out</p>
+      </div>
+    </>
+  );
+};
+
+export default Closed;
