d56d76619d68e6a646db42344dbccd1609f88b36
Jack <jack200034@hotmail.com>
2023-03-27 00:15:27 +0100

    Import icons & style items

--- src/components/sidebars/Open.tsx
+++ src/components/sidebars/Open.tsx
@@ -1,21 +1,52 @@
 import React from "react";
 
+import { MdOutlineLogout, MdOutlineFeed } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { VscFeedback } from "react-icons/vsc";
+import { GrHelp } from "react-icons/gr";
+import { BsCalendar3, BsBell } from "react-icons/bs";
+import { IoMdStats } from "react-icons/io";
+
 const Open = () => {
   return (
     <>
-      <div>
-        <p>Notifications</p>
-        <p>Feed</p>
-        <p>Stats</p>
-        <p>Calendar</p>
+      <div className="pb-16 space-y-2">
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <BsBell />
+          <p>Notifications</p>
+        </div>
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <MdOutlineFeed />
+          <p>Feed</p>
+        </div>
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <IoMdStats />
+          <p>Stats</p>
+        </div>
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <BsCalendar3 />
+          <p>Calendar</p>
+        </div>
       </div>
-      <div>
-        <p>Get Help</p>
-        <p>Submit Feedback</p>
+      <div className="pb-16 space-y-2">
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <GrHelp />
+          <p>Get Help</p>
+        </div>
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <VscFeedback />
+          <p>Submit Feedback</p>
+        </div>
       </div>
-      <div>
-        <p>Profile</p>
-        <p>Sign Out</p>
+      <div className="space-y-2">
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <CgProfile />
+          <p>Profile</p>
+        </div>
+        <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md hover:bg-slate-300/20">
+          <MdOutlineLogout />
+          <p>Sign Out</p>
+        </div>
       </div>
     </>
   );
