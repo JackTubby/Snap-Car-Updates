1fe7b7c7616cd2cd16ba88833055d2f0ec6cd0c1
JackTubby <jack200034@hotmail.com>
2023-03-29 23:53:26 +0100

    Add sidebar heading

--- src/components/sidebars/Sidebar.tsx
+++ src/components/sidebars/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <div className="h-screen w-64 border-r-2 border-black">
       <div className="text-center p-8">
-
+        <h2 className="pb-16">Snap Car Updates</h2>
       </div>
     </div>
   );
