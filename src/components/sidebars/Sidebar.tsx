4cda3c254c2b4b12c633264f10c510edcb697e56
JackTubby <jack200034@hotmail.com>
2023-03-30 01:10:34 +0100

    Format file

--- src/components/sidebars/Sidebar.tsx
+++ src/components/sidebars/Sidebar.tsx
@@ -4,16 +4,21 @@ import Open from "./Open";
 import Closed from "./Closed";
 
 const Sidebar = () => {
-
   const [isShown, setIsShown] = useState(false);
   const sidebarWidth = isShown ? "w-64" : "w-36";
 
   return (
-    <div onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} className="bg-blue-300 w-80">
-      <div className={`h-screen ${sidebarWidth} border-r-2 border-black`}>
+    <>
+      <div
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+        className="w-80"
+      >
+        <div className={`h-screen ${sidebarWidth} border-r-2 border-black`}>
           {isShown ? <Open /> : <Closed />}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
