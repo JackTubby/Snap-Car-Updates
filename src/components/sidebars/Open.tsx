0bc1f34475a7f6e7ec632fe1e427cd3df9b6337d
JackTubby <jack200034@hotmail.com>
2023-03-30 01:04:18 +0100

    Add transition to sidebar items

--- src/components/sidebars/Open.tsx
+++ src/components/sidebars/Open.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
 import { MdOutlineLogout, MdOutlineFeed } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { VscFeedback } from "react-icons/vsc";
@@ -9,53 +11,90 @@ import { IoMdStats } from "react-icons/io";
 const Open = () => {
   return (
     <>
-      <div className="pb-16 space-y-2 text-center p-8">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="pb-16 space-y-2 text-center p-8"
+      >
         <h2 className="pb-16">Snap Car Updates</h2>
         <>
           <div className="pb-16 space-y-2">
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <BsBell />
               <p>Notifications</p>
-            </div>
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <MdOutlineFeed />
               <p>Feed</p>
-            </div>
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <IoMdStats />
               <p>Stats</p>
-            </div>
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <BsCalendar3 />
               <p>Calendar</p>
-            </div>
+            </motion.div>
           </div>
         </>
         <>
           <div className="pb-16 space-y-2">
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <BsQuestionLg />
               <p>Get Help</p>
-            </div>
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <VscFeedback />
               <p>Submit Feedback</p>
-            </div>
+            </motion.div>
           </div>
         </>
         <>
           <div className="space-y-2">
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <CgProfile />
               <p>Profile</p>
-            </div>
-            <div className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300">
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center space-x-2 p-2 cursor-pointer rounded-md text-gray-600 dark:text-gray-400 dark:hover:bg-slate-300/20 hover:bg-slate-300"
+            >
               <MdOutlineLogout />
               <p>Sign Out</p>
-            </div>
+            </motion.div>
           </div>
         </>
-      </div>
+      </motion.div>
     </>
   );
 };
