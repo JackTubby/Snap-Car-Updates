42c47d46b02d9038eeea3d6f5d3ea1860eb68a2c
Jack <jack200034@hotmail.com>
2023-04-02 23:53:03 +0100

    Add heading & mssg and add form components

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,15 @@
+import SignupForm from "../components/auth-forms/SignupForm";
+
 const Signup = () => {
-    return (
-      <div></div>
-    );
-  };
-  
-  export default Signup;
+  return (
+    <>
+      <h2>Create an Account</h2>
+      <p>Let's get started with your 30 day free trial.</p>
+      {/* Form for Signup */}
+        <SignupForm />
+      <a>Click here to login.</a>
+    </>
+  );
+};
+
+export default Signup;
