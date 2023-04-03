de17ef2b669776da07c922be28e2293be1613f04
Jack <jack200034@hotmail.com>
2023-04-04 00:50:43 +0100

    Import login & signup - add to content

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,7 +1,11 @@
+import Login from "./Login"
+import Signup from "./Signup"
+
 const AuthPage = () => {
+    const loginOrSignup = true;
     return (
         <div className="flex w-full text-center">
-            <div className="w-6/12 h-screen relative top-64">a</div>
+            <div className="w-6/12 h-screen relative top-64">{ loginOrSignup ? <Login /> : <Signup /> }</div>
             <div className="w-6/12 h-screen bg-red-500"></div>
         </div>
     )
