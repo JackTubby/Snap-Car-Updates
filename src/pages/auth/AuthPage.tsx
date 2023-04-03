e31cffc4b78422392a290292fd34bc4584001b78
Jack <jack200034@hotmail.com>
2023-04-04 00:48:04 +0100

    Init & create 50/50 split screen

---
+++ src/pages/auth/AuthPage.tsx
@@ -0,0 +1,10 @@
+const AuthPage = () => {
+    return (
+        <div className="flex w-full text-center">
+            <div className="w-6/12 h-screen relative top-64">a</div>
+            <div className="w-6/12 h-screen bg-red-500"></div>
+        </div>
+    )
+}
+
+export default AuthPage
