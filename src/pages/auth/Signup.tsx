dcd728c05c78740783c9726d98f1aa798fb14a43
JackTubby <jack200034@hotmail.com>
2023-04-05 00:14:34 +0100

    Change ' to &apos; to fix error

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -5,7 +5,7 @@ const Signup = () => {
     <>
       <h2 className="pb-3 text-3xl">Create an Account</h2>
       <p className="pb-8 text-sm">
-        Let's get started with your 30 day free trial.
+        Let&apos;s get started with your 30 day free trial.
       </p>
       {/* Form for Signup */}
       <div className="py-14">
