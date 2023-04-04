16d5e2ebb38c83b0d9c4a668d56bd8c324a7812d
JackTubby <jack200034@hotmail.com>
2023-04-05 00:13:09 +0100

    Fix styling pushing screen down

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,14 +1,17 @@
-import Login from "./Login"
-import Signup from "./Signup"
+import Login from "./Login";
+import Signup from "./Signup";
 
 const AuthPage = () => {
-    const loginOrSignup = true;
-    return (
-        <div className="flex w-full text-center">
-            <div className="w-6/12 h-screen relative top-64">{ loginOrSignup ? <Login /> : <Signup /> }</div>
-            <div className="w-6/12 h-screen bg-red-500"></div>
-        </div>
-    )
-}
+  const loginOrSignup = false;
+  return (
+    <div className="flex w-full text-center">
+      <div className="w-6/12 h-screen relative pt-64">
+        {loginOrSignup ? <Login /> : <Signup />}
+      </div>
+      <div className="w-6/12 h-screen bg-red-500">
+      </div>
+    </div>
+  );
+};
 
-export default AuthPage
+export default AuthPage;
