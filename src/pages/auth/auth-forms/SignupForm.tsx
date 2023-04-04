250e42cd7d5307276864d701f50d3add6586733b
JackTubby <jack200034@hotmail.com>
2023-04-05 00:14:04 +0100

    Move to auth & style forms

---
+++ src/pages/auth/auth-forms/SignupForm.tsx
@@ -0,0 +1,48 @@
+const LoginForm = () => {
+  return (
+    <>
+      <form className="flex justify-center">
+        <div className="w-1/2">
+          <div className="p-4 mt-4">
+            <input
+              type="text"
+              placeholder="Name"
+              className="border-b-2 
+              w-full 
+              outline-none 
+              focus:border-custom-purple 
+              focus:ease-in 
+              focus:duration-200"
+            />
+          </div>
+          <div className="p-4 mt-4">
+            <input
+              type="text"
+              placeholder="Email"
+              className="border-b-2 
+              w-full 
+              outline-none 
+              focus:border-custom-purple 
+              focus:ease-in 
+              focus:duration-200"
+            />
+          </div>
+          <div className="p-4 mt-4">
+            <input
+              type="text"
+              placeholder="Password"
+              className="border-b-2 
+              w-full 
+              outline-none 
+              focus:border-custom-purple 
+              focus:ease-in 
+              focus:duration-200"
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
