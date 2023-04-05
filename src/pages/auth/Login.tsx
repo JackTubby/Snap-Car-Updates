9c6ab926e5111a77142bdd1ae2512ebe13894f57
JackTubby <jack200034@hotmail.com>
2023-04-05 21:53:19 +0100

    Move auth page layout into file

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,16 +1,21 @@
-import LoginForm from "./auth-forms/LoginForm";
+import LoginForm from "../../components/auth-forms/LoginForm";
+
+import Image from "next/image";
+import garageImg from "../../../public/assets/img/garage.jpg";
 
 const Login = () => {
   return (
-    <div>
-      <h2 className="pb-3 text-3xl">Welcome Back</h2>
-      <p className="pb-8 text-sm">Please login to access your account.</p>
-      {/* Login Form */}
-      <div className="py-14">
-        <LoginForm />
-      </div>
-      <button
-        className="w-1/2 p-2 
+    <div className="flex w-full text-center">
+      <div className="w-6/12 h-screen relative pt-64">
+        <div>
+          <h2 className="pb-3 text-3xl">Welcome Back</h2>
+          <p className="pb-8 text-sm">Please login to access your account.</p>
+          {/* Login Form */}
+          <div className="py-14">
+            <LoginForm />
+          </div>
+          <button
+            className="w-1/2 p-2 
                 bg-black 
                 text-white 
                 dark:bg-white 
@@ -19,10 +24,25 @@ const Login = () => {
                 dark:hover:bg-custom-purple 
                 hover:ease-in 
                 hover:duration-100"
-      >
-        Log In
-      </button>
-      <button className="hover:text-custom-purple hover:border-none border-none dark:text-white dark:hover:text-custom-purple">Haven&apos;t got an account? Create one here.</button>
+          >
+            Log In
+          </button>
+          <div className="mt-4">
+            <button className="hover:text-custom-purple hover:border-none border-none dark:text-white dark:hover:text-custom-purple">
+              Haven&apos;t got an account? Create one here.
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-6/12 h-screen">
+        <div className="w-5/6 h-5/6 m-auto p-2 border-2 border-custom-purple rounded-2xl">
+          <Image
+            src={garageImg}
+            alt="Image of a car workshop"
+            className="h-full rounded-2xl"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,18 +1,23 @@
-import SignupForm from "./auth-forms/SignupForm";
+import SignupForm from "../../components/auth-forms/SignupForm";
+
+import Image from "next/image";
+import garageImg from "../../../public/assets/img/garage.jpg";
 
 const Signup = () => {
   return (
     <>
-      <h2 className="pb-3 text-3xl">Create an Account</h2>
-      <p className="pb-8 text-sm">
-        Let&apos;s get started with your 30 day free trial.
-      </p>
-      {/* Form for Signup */}
-      <div className="py-14">
-        <SignupForm />
-      </div>
-      <button
-        className="w-1/2 p-2 
+      <div className="flex w-full text-center">
+        <div className="w-6/12 h-screen relative pt-64">
+          <h2 className="pb-3 text-3xl">Create an Account</h2>
+          <p className="pb-8 text-sm">
+            Let&apos;s get started with your 30 day free trial.
+          </p>
+          {/* Form for Signup */}
+          <div className="py-14">
+            <SignupForm />
+          </div>
+          <button
+            className="w-1/2 p-2 
                 bg-black 
                 text-white 
                 dark:bg-white 
@@ -21,11 +26,24 @@ const Signup = () => {
                 dark:hover:bg-custom-purple 
                 hover:ease-in 
                 hover:duration-100"
-      >
-        Sign up
-      </button>
-      <div className="mt-4">
-        <button className="hover:text-custom-purple hover:border-none border-none dark:text-white dark:hover:text-custom-purple">Already got an account? Click here to login.</button>
+          >
+            Sign up
+          </button>
+          <div className="mt-4">
+            <button className="hover:text-custom-purple hover:border-none border-none dark:text-white dark:hover:text-custom-purple">
+              Already got an account? Click here to login.
+            </button>
+          </div>
+        </div>
+        <div className="flex w-6/12 h-screen">
+          <div className="w-5/6 h-5/6 m-auto p-2 border-2 border-custom-purple rounded-2xl">
+            <Image
+              src={garageImg}
+              alt="Image of a car workshop"
+              className="h-full rounded-2xl"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
