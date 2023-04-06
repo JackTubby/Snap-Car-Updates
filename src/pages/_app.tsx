7ef39a9ce8a0fd5bbef9cc4e5afa1b7076b9d487
JackTubby <jack200034@hotmail.com>
2023-04-06 15:40:50 +0100

    Add check to see if auth state changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,37 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
+import React, { useState, useEffect } from "react";
+import Firebase from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+
+const formatAuthUser = (user:any) => ({
+  uid: user.uid,
+  email: user.email
+});
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+
+  // console.log(auth)
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        console.log("uid", uid);
+      } else {
+        // User is signed out
+        console.log("user is logged out");
+      }
+    });
+  }, []);
   return (
     // Attribute class tells Tailwind to use the dark theme class
-    <ThemeProvider attribute="class"> 
+    <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
